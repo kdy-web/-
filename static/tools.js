46b7b3db023eed78fd27d56dab6151fb5cd0130f
focus <focuschen18@gmail.com>
2017-12-19 18:53:26 +0800

    Initial commit

---
+++ static/tools.js
@@ -0,0 +1,29 @@
+
+function arr_num(arr) {
+	var len = arr.length;
+	for(var i = 0; i < len; i++) {
+		arr[i] = Number(arr[i]);
+
+	}
+	return arr;
+}
+
+
+
+
+				
+
+function getGrade() {
+					
+				}
+
+function getMinGrade() {
+				
+				}
+
+export{
+	arr_num,
+
+	getGrade,
+	getMinGrade
+}
